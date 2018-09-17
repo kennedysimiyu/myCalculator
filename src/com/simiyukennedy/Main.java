f4a5cc36ad67ae83575631c7d1ced33697128fef
Ken Simiyu <kensimiyu2015@gmail.com>
2018-09-17 17:03:19 +0300

    replacing an else statement with a switch

--- src/com/simiyukennedy/Main.java
+++ src/com/simiyukennedy/Main.java
@@ -8,26 +8,31 @@ public class Main {
         double [] leftVals = {100.0d, 200.0d, 240.0d, 456.0d};
         double [] rightVals = {238.0d, 365.0d, 173.0d, 296.0d};
 
-        double [] opCodes = { 'a', 's', 'm', 'd'};
+        char [] opCodes = { 'a', 's', 'm', 'd'};
         double [] results = new double[opCodes.length];
 
         //double input1 = 20.0d;
         //double input2 = 30.0d;
         //double result;
         //char opCode = 'a';
-        for(int i=0; i<opCodes.length; i++){
-            if (opCodes[i] == 'a')
-                results[i] = leftVals[i] + rightVals[i];
-            else if (opCodes[i] == 's')
-                results[i] = leftVals[i] - rightVals[i];
-            else if (opCodes[i] == 'm')
-                results[i] = leftVals[i] * rightVals[i];
-            else if (opCodes[i] == 'd') {
-                results[i] = leftVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
-            } else {
-                System.out.println("Error - invalid opCode");
-                results[i] = 0.0d;
-
+        for(int i = 0; i < opCodes.length; i++) {
+            switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = leftVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
+                default:
+                    System.out.println("Error - invalid opCode");
+                    results[i] = 0.0d;
+                    break;
             }
         }
         for(double theResult: results){
